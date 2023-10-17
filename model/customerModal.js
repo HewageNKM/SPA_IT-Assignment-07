586084d7a6b8f8167e9dbbf161da95802d3678be
kawishika <hnkmalwenna@gmail.com>
2023-10-17 16:18:05 +0530

    js finalized

---
+++ model/customerModal.js
@@ -0,0 +1,10 @@
+
+function customerModal(customerID,customerName,customerAddress,customerSalary) {
+    var customer={
+        cusId:customerID,
+        cusName:customerName,
+        cusAddress:customerAddress,
+        cusSalary:customerSalary
+    }
+    customerAr.push(customer);
+}
